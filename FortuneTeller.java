f1223d34fae9e842ff04e371632716a02e71c10f
sardor <alimukhammad1@gmail.com>
2023-04-01 20:38:12 -0400

    more fortunes were added into the array list

--- FortuneTeller.java
+++ FortuneTeller.java
@@ -22,7 +22,17 @@ public class FortuneTeller{
             "The stars are aligned in your favor.",
             "You will soon embark on a great adventure.",
             "Good things come to those who wait.",
-            "You will meet a stranger who will change your life."
+            "You will meet a stranger who will change your life.",
+            "You will travel to a new place and discover something amazing.",
+            "Your hard work will pay off and bring you success.",
+            "Someone special will come into your life soon.",
+            "A great opportunity is headed your way - be ready to seize it.",
+            "You will overcome a challenge and emerge stronger than before.",
+            "Your creativity and passion will lead you to great accomplishments.",
+            "Your kindness and generosity will be rewarded in unexpected ways.",
+            "You will make a new friend who will become an important part of your life.",
+            "A major change is coming, but it will ultimately be for the better.",
+            "You will receive unexpected help from someone when you need it most."
         };
 
         int fortuneIndex = rand.nextInt(fortunes.length);
